899585e5d8f1f2c22041297b8bc3eb29e753ec4a
buzum <buzum@mail.ru>
2023-03-15 21:49:07 +0400

    task 34 for HW 5

---
+++ task34/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//31. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+//Найдите сумму отрицательных и положительных элементов массива.
+Console.Clear();
+int UserNumber = GetNumberFromUser("Задайте количество элементов в массиве: ","Ошибка ввода!");
+int[] array = GetArray(UserNumber, 100, 999);
+//Console.WriteLine(String.Join(" ", array));
+int positiveSum = GetEvenCount(array);
+
+Console.WriteLine($"[{String.Join(", ", array)}] -> {positiveSum}");
+//////////////////////////////////////////////////////////////////////////////////
+
+// Выводит в консоль сообщение message,
+// преобразовывает введённую пользователем строку в число типа int.
+// В случае ошибки выводит в консоль сообщение errorMessage
+int GetNumberFromUser(string message, string errorMessage)
+{ 
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect) return userNumber;
+        Console.WriteLine(errorMessage);
+    } 
+}
+
+// Возвращает массив из size элементов,
+// заполненный случайными числами из промежутка [minValue, maxValue]
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+
+// Возвращает сумму положительных чисел массива arr
+int GetEvenCount(int[] arr)
+{
+    int evenCount = 0;
+    foreach (int el in arr)
+    {
+        if (el % 2 == 0) evenCount += 1;
+    }
+    return evenCount;
+}
